da6a9aee753cd3ae68eb3cd5dd9a2ba692de4972
LSenzaki <108425677+LSenzaki@users.noreply.github.com>
2025-08-29 20:03:23 -0300

    Melhoria na estruturacao

--- app/main.py
+++ app/main.py
@@ -3,4 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 from auth_routes import auth_router
+
+app.include_router(auth_router)
+
 # Para rodar, executar no terminarl -> uvicorn main:app --reload
