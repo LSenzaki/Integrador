14cafd786bd9c4212570594533f28bb0563e0b96
Lucas Senzaki <lucassenzaki@gmail.com>
2025-09-04 16:02:46 -0300

    Ajuste na rota raiz

--- app/main.py
+++ app/main.py
@@ -31,4 +31,5 @@ app.include_router(faces.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    from app.main import app
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
